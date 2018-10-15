d7677dbfd93e83865d0e8569c5de9d2c2b596e0a
903174293 <903174293@qq.com>
2018-10-15 10:30:05 +0800

    Initial commit

---
+++ mavenproject/src/main/java/cn/mldn/zwb/mavenproject/MavenprojectApplication.java
@@ -0,0 +1,12 @@
+package cn.mldn.zwb.mavenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenprojectApplication.class, args);
+    }
+}
